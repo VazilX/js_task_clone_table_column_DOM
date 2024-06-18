d5c070af0d36eab25bbe34708069367557e50652
Vasil Liakhivskyi <vazilx@gmail.com>
2024-06-18 08:24:20 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,22 @@
+"use strict";
+const table = document.querySelector("table");
+const rows = [
+    ...table.rows
+];
+rows.forEach((row)=>{
+    const cells = [
+        ...row.cells
+    ];
+    const cloneSecondCell = cells[1].cloneNode(true);
+    const firstPartCells = cells.slice(0, -1);
+    const newCells = [
+        ...firstPartCells,
+        cloneSecondCell,
+        cells.at(-1)
+    ];
+    newCells.forEach((cell)=>{
+        row.append(cell);
+    });
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
